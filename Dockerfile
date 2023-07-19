e91fc00c183bddd0687db94a2cc3734605151151
Gavin Brown <gav@greycubes.net>
2023-07-19 18:26:55 +0300

    feat!: Major changes to add stunnel wrapper, passfile, simplify env vars in use Removes option to set share name, sets to /vault as standard Added base stunnel.conf file Added base pem.conf file Rewrite to entrypoint.sh  * Added stunnel wrapper  * Changed environment password variable to use tmp in memory file  * Added option to mount password file in container instead Updated dot_env_sample to include new env variables Updated dot_env_sample to remove CRYPTOMATOR_VAULT_NAME Updated Dockerfile to prebuild stunnel and self signed pem cert Updated docker-compose.yml to add passfile variable and new dev option

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,47 @@
+#------------------------------------------------------------------------------------------
+# Stage 1: Build container
+#------------------------------------------------------------------------------------------
+FROM redhat/ubi9 as builder
+
+# Install build dependencies
+RUN yum install -y gcc make openssl-devel
+
+# Download and build stunnel
+RUN curl -LO https://www.stunnel.org/downloads/stunnel-5.70.tar.gz \
+    && tar -xzf stunnel-5.70.tar.gz \
+    && cd stunnel-5.70 \
+    && ./configure \
+    && make \
+    && make install
+
+# Create a new selfsigned certificate
+COPY config/pem.conf /root/pem.conf
+RUN openssl req -newkey rsa:2048 -nodes -keyout /root/stunnel.pem -x509 -days 3650 -out /root/stunnel.pem -config /root/pem.conf
+
+#------------------------------------------------------------------------------------------
+# Stage 2: Final container
+#------------------------------------------------------------------------------------------
 FROM eclipse-temurin:17.0.7_7-jre-ubi9-minimal
+
+# Copy stunnel binary from the builder stage
+COPY --from=builder /usr/local/bin/stunnel /usr/local/bin/stunnel
 
 ENV CRYPTOMATOR_VAULT_NAME='vault'
 ENV CRYPTOMATOR_SRC_PATH='/path/to/cryptomator/vault/files'
 ENV CRYPTOMATOR_VAULT_PASS='password'
-ENV CRYPTOMATOR_PORT='18081'
 ENV CRYPTOMATOR_UID='1000'
 ENV CRYPTOMATOR_GID='1000'
 
-EXPOSE ${CRYPTOMATOR_PORT}
+EXPOSE 8443
 
+# Createa  local cryptomator user and group to keep files contained to local user
 RUN groupadd -g "${CRYPTOMATOR_GID}" cryptomator && useradd --no-log-init -u "${CRYPTOMATOR_UID}" -g cryptomator cryptomator
 
 COPY --chown=cryptomator:cryptomator --chmod=0440 packages/cryptomator-cli-latest.jar /usr/local/bin/cryptomator-cli.jar
+COPY --chown=cryptomator:cryptomator --chmod=0440 config/stunnel.conf /usr/local/etc/stunnel/stunnel.conf
+COPY --from=builder --chown=cryptomator:cryptomator --chmod=0440 /root/stunnel.pem /usr/local/etc/stunnel/stunnel.pem
+
+# Copy over the main entrypoint script last (to speed up rebuilds)
 COPY --chown=cryptomator:cryptomator --chmod=0550 entrypoint.sh /entrypoint.sh
 
 USER cryptomator
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,65 @@
 #!/usr/bin/env bash
 
-JAR_LOCATION='/usr/local/bin/cryptomator-cli.jar'
-CRYPTOMATOR_PASSFILE_LOC='/tmp/vault_pass' # ToDo: could this be read from the container's filesystem in the docker host?
-CRYPTOMATOR_BIND='0.0.0.0'
-
-# Write the password to a tmp file to prevent it appearing in the process list
-if [[ ! -f "${CRYPTOMATOR_PASSFILE_LOC}" ]]; then
-    touch "${CRYPTOMATOR_PASSFILE_LOC}"
-    echo "${CRYPTOMATOR_VAULT_PASS}" > "${CRYPTOMATOR_PASSFILE_LOC}"
-    chmod 0400 "${CRYPTOMATOR_PASSFILE_LOC}"
-fi
+sleep_with_dots() {
+  local seconds=$1
+  for ((i = 0; i < seconds; i++)); do
+    sleep 1
+    echo "Waiting... " $((i + 1))
+  done
+}
 
 # Show the current UID and GID of the running process
-echo "Current UIDs/GIDs:"
-cat /proc/self/status | grep '[UG]id'
+echo "Currently running as UIDs/GIDs:"
+grep '[UG]id' < '/proc/self/status'
+
 # Set the umask to ensure files are not created world readable
 umask 0026
-echo "Current umask: $(umask)"
+echo "With umask of: $(umask)"
 
-echo "Starting cryptomator-cli, will listen on: http://${CRYPTOMATOR_BIND}:${CRYPTOMATOR_PORT}/${CRYPTOMATOR_VAULT_NAME}"
+CRYPTOMATOR_PASSFILE=0 # Assume we've not been given a passfile to work with
+CRYPTOMATOR_VAULT_PASSFILE='/vault.pass'
+
+# If we've been given a vault password file location, use it, else use a default which we'll create from the environment env
+if [[ -f "${CRYPTOMATOR_VAULT_PASSFILE}" ]]; then
+    if [[ ! -r "${CRYPTOMATOR_VAULT_PASSFILE}" ]]; then
+        CRYPTOMATOR_PASSFILE=1
+        echo "Error: ${CRYPTOMATOR_VAULT_PASSFILE} is mounted but is not readable:" >&2
+        echo "Error: Attempting to stat file..." >&2
+        stat "${CRYPTOMATOR_VAULT_PASSFILE}" >&2
+        echo "Exiting...."
+        exit 1
+    fi
+    echo "Using mounted passfile"
+    CRYPTOMATOR_INTERNAL_PASSFILE_LOC="$CRYPTOMATOR_VAULT_PASSFILE"
+else
+    # We've not been given a password file so assume we were given one in the environment variables
+    echo "Using password provided in environment"
+    CRYPTOMATOR_INTERNAL_PASSFILE_LOC='/dev/shm/cryptomator_vault_pass'
+    echo "Creating CRYPTOMATOR_INTERNAL_PASSFILE_LOC file at $CRYPTOMATOR_INTERNAL_PASSFILE_LOC"
+    install -o cryptomator -g cryptomator -m 0600 /dev/null "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
+
+    echo "Writing password to file $CRYPTOMATOR_INTERNAL_PASSFILE_LOC"
+    echo "${CRYPTOMATOR_VAULT_PASS}" > "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
+
+    # Bank out the password from the environment
+    export -n CRYPTOMATOR_VAULT_PASS
+fi
+
+# Start cryptomator-cli listening on localhost
+echo 'Starting cryptomator-cli in background, will share on: http://127.0.0.1:8080/vault/'
 echo "------------------------------"
-exec java -XX:-UsePerfData -jar "${JAR_LOCATION}" --bind="${CRYPTOMATOR_BIND}" --port="${CRYPTOMATOR_PORT}" \
-    --vault "${CRYPTOMATOR_VAULT_NAME}=/vault"  \
-    --passwordfile "${CRYPTOMATOR_VAULT_NAME}=${CRYPTOMATOR_PASSFILE_LOC}"
+java -XX:-UsePerfData -jar '/usr/local/bin/cryptomator-cli.jar' --bind='127.0.0.1' --port='8080' \
+    --vault "vault=/vault"  \
+    --passwordfile "vault=${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}" &
+
+# If we were not given a CRYPTOMATOR_VAULT_PASSFILE, we clean up our temporary file
+if [[ "$CRYPTOMATOR_PASSFILE" == 0 ]]; then
+    echo "Waiting for cryptomator-cli to start..."
+    sleep_with_dots 5
+    echo "Removing temporary pass file ${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
+    rm -f "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
+fi
+
+# Start stunnel to wrap the cryptomator-cli webdav in a TLS tunnel and export on container lan ip
+echo "Starting stunnel, will share on: https://0.0.0.0:8443/vault/"
+exec /usr/local/bin/stunnel
