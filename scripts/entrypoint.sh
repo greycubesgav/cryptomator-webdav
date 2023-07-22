7bf2f187d459f3fb01ea17122d1fdc93e3cf3787
Gavin Brown <gav@greycubes.net>
2023-07-23 00:08:35 +0300

    feat!: Rewrite of init scripts in 'sh' * Updated calls to echo -e to printf * Update if statements to match 'sh' format * Add full paths to java and stunnel * Add trap to init.sh to fix docker stop clean shutdown

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,52 +1,60 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC1072,SC2120
+#!/usr/bin/env sh
+##!/usr/bin/env bash
+# shellcheck disable=SC1072,SC2120,SC2059
 
 sleep_with_dots() {
-  local seconds=$1
-  for ((i = 0; i < seconds; i++)); do
+  msg="$1"
+  seconds="$2"
+  i=0
+  while [ "$i" -lt "$seconds" ]; do
     sleep 1
-    echo "Waiting... " $((i + 1))
+    echo "${msg}$((i + 1))"
+    i=$((i + 1))
   done
 }
 
+# Define steps to clean up container parts when exiting
 cleanup() {
-    echo "Received signal: $1"
-    if [[ -n $CRYPTOMATOR_PID ]]; then
-        echo "Cleaning up cryptomator-cli (PID: $CRYPTOMATOR_PID)"
-        kill "$CRYPTOMATOR_PID"
-    fi
-    if [[ -n $STUNNEL_PID ]]; then
-        echo "Cleaning up stunnel (PID: $STUNNEL_PID)"
-        kill "$STUNNEL_PID"
-    fi
-    exit 0
+  echo "Received signal: $1"
+  if [ -n "$CRYPTOMATOR_PID" ]; then
+    echo "Cleaning up cryptomator-cli (PID: $CRYPTOMATOR_PID)"
+    kill -s TERM "$CRYPTOMATOR_PID"
+  fi
+  if [ -n "$STUNNEL_PID" ]; then
+    echo "Cleaning up stunnel (PID: $STUNNEL_PID)"
+    kill -s TERM "$STUNNEL_PID"
+  fi
+  echo 'Exiting entrypoint....'
+  exit 0
 }
 
 # Trap signals to call the cleanup function
-trap 'cleanup SIGTERM' SIGTERM
-trap 'cleanup SIGHUP' SIGHUP
-trap 'cleanup SIGINT' SIGINT
+trap 'cleanup SIGTERM' TERM
+trap 'cleanup SIGHUP' HUP
+trap 'cleanup SIGINT' INT
 
+#------------------------------------------------------------------------------------------------------------------------------
+# Ouput status information on the current user/group and umask
 CUR_UMASK=$(umask)
 CUR_UID=$(setpriv -d | grep '^uid' | awk '{print $2}')
 CUR_GID=$(setpriv -d | grep '^gid' | awk '{print $2}')
 
-echo -e "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}"
-echo -e "${C_GREEN}#${C_NC} uid: ${C_GREEN}${CUR_UID}${C_NC} gid: ${C_GREEN}${CUR_GID}${C_NC} umask: ${C_GREEN}${CUR_UMASK}${C_NC}"
-echo -e "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}"
-echo -e "${C_GREEN}#${C_NC} cryptomator-cli listening on                   : ${C_MAGENTA}webdav://127.0.0.1:8080${C_NC}"
-echo -e "${C_GREEN}#${C_NC} stunnel listening on                           : ${C_MAGENTA}https://0.0.0.0:8443${C_NC}"
-echo -e "${C_GREEN}#${C_NC} TLS secured webdav cryptomator vault access on : ${C_GREEN}webdavs://containerIP:8443/vault${C_NC}"
-echo -e "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}"
+printf "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}\n"
+printf "${C_GREEN}#${C_NC} uid: ${C_GREEN}%s${C_NC} gid: ${C_GREEN}%s${C_NC} umask: ${C_GREEN}%s${C_NC}\n" "$CUR_UID" "$CUR_GID" "$CUR_UMASK"
+printf "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}\n"
+printf "${C_GREEN}#${C_NC} cryptomator-cli listening on                   : ${C_MAGENTA}%s${C_NC}\n" "webdav://127.0.0.1:8080"
+printf "${C_GREEN}#${C_NC} stunnel listening on                           : ${C_MAGENTA}%s${C_NC}\n" "https://0.0.0.0:8443"
+printf "${C_GREEN}#${C_NC} TLS secured webdav cryptomator vault access on : ${C_GREEN}%s${C_NC}\n" "webdavs://containerIP:8443/vault"
+printf "${C_GREEN}#----------------------------------------------------------------------------------------${C_NC}\n"
 
 #------------------------------------------------------------------------------------------------------------------------------
 
 CRYPTOMATOR_PASSFILE=0 # Assume we've not been given a passfile to work with
-CRYPTOMATOR_VAULT_PASSFILE='/vault.pass'
+CRYPTOMATOR_VAULT_PASSFILE='/vault.pass' # Set the default local location where we expect the vault password file to be mounted
 
 # If we've been given a vault password file location, use it, else use a default which we'll create from the environment env
-if [[ -f "${CRYPTOMATOR_VAULT_PASSFILE}" ]]; then
-    if [[ ! -r "${CRYPTOMATOR_VAULT_PASSFILE}" ]]; then
+if [ -f "${CRYPTOMATOR_VAULT_PASSFILE}" ]; then
+    if [ ! -r "${CRYPTOMATOR_VAULT_PASSFILE}" ]; then
         echo "Error: ${CRYPTOMATOR_VAULT_PASSFILE} is mounted but is not readable:" >&2
         echo "Error: Attempting to stat file for debug purposes..." >&2
         stat "${CRYPTOMATOR_VAULT_PASSFILE}" >&2
@@ -58,7 +66,7 @@ if [[ -f "${CRYPTOMATOR_VAULT_PASSFILE}" ]]; then
     CRYPTOMATOR_INTERNAL_PASSFILE_LOC="$CRYPTOMATOR_VAULT_PASSFILE"
 else
     # We've not been given a password file so assume we were given one in the environment variables
-    echo 'Using password provided in environment variable $CRYPTOMATOR_VAULT_PASS'
+    echo "Using password provided in environment variable ${CRYPTOMATOR_VAULT_PASS}"
     CRYPTOMATOR_INTERNAL_PASSFILE_LOC='/dev/shm/cryptomator_vault_pass'
     echo "Creating CRYPTOMATOR_INTERNAL_PASSFILE_LOC file at $CRYPTOMATOR_INTERNAL_PASSFILE_LOC"
     install -o cryptomator -g cryptomator -m 0600 /dev/null "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
@@ -67,34 +75,36 @@ else
     echo "${CRYPTOMATOR_VAULT_PASS}" > "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
 
     # Bank out the password from the environment
-    export -n CRYPTOMATOR_VAULT_PASS
+    CRYPTOMATOR_VAULT_PASS=''
+    export CRYPTOMATOR_VAULT_PASS
 fi
 
 # Start cryptomator-cli listening on localhost
-echo '#----------------------------------------------------------------------------------------'
 echo 'Starting cryptomator-cli in background, will share on: webdav://127.0.0.1:8080/vault/'
-echo '#----------------------------------------------------------------------------------------'
-java -XX:-UsePerfData -jar '/usr/local/bin/cryptomator-cli.jar' --bind='127.0.0.1' --port='8080' \
+
+# Note: Currenly hardcoded path for alpine java location
+/usr/bin/java -XX:-UsePerfData -jar '/usr/local/bin/cryptomator-cli.jar' --bind='127.0.0.1' --port='8080' \
     --vault "vault=/vault"  \
     --passwordfile "vault=${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}" &
 CRYPTOMATOR_PID=$!
 echo "cryptomator-cli PID: ${CRYPTOMATOR_PID}"
 
-# If we were not given a CRYPTOMATOR_VAULT_PASSFILE, we clean up our temporary file
-if [[ "$CRYPTOMATOR_PASSFILE" == 0 ]]; then
+# If we were not given a CRYPTOMATOR_VAULT_PASSFILE, we give time for cryptomator-cli to start, then clean up our temporary file
+if [ "$CRYPTOMATOR_PASSFILE" = 0 ]; then
     echo "Waiting for cryptomator-cli to start..."
-    sleep_with_dots 5
+    sleep_with_dots "Waiting for cryptomator-cli... " 5
     echo "Removing temporary pass file ${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
     rm -f "${CRYPTOMATOR_INTERNAL_PASSFILE_LOC}"
 fi
+echo '#-------------------------------------------------------'
 
 # Start stunnel to wrap the cryptomator-cli webdav in a TLS tunnel and export on container lan ip
 echo "Starting stunnel, TLS tunneling 127.0.0.1:8080 => 0.0.0.0:8443"
-echo '#----------------------------------------------------------------------------------------'
-/usr/local/bin/stunnel &
+/usr/bin/stunnel /etc/stunnel/stunnel.conf &
 STUNNEL_PID=$!
 echo "stunnel PID: ${STUNNEL_PID}"
+echo '#-------------------------------------------------------'
 
 # Wait for the stunnel command to finish, run the cleanup if it dies
 wait $STUNNEL_PID
-cleanup
+cleanup EXIT
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+# shellcheck disable=SC2059
+
+cleanup() {
+  echo "Received signal: $1"
+  if [ -n "$CRYPTOMATOR_ENTRYPOINT_PID" ]; then
+    echo "Cleaning up entrypoint.sh (PID: $CRYPTOMATOR_ENTRYPOINT_PID)"
+    kill -s TERM "$CRYPTOMATOR_ENTRYPOINT_PID"
+  fi
+  echo 'Exiting init....'
+  exit 0
+}
+
+# Trap signals to call the cleanup function
+trap 'cleanup SIGTERM' TERM
+trap 'cleanup SIGHUP' HUP
+trap 'cleanup SIGINT' INT
 
 CRYPTOMATOR_USER='cryptomator'
+
+
 
 #------------------------------------------------------------------------------------------------------------------------------
 # Header
@@ -10,35 +28,39 @@ export C_GREEN='\033[0;32m'
 export C_MAGENTA='\033[0;35m'
 export C_NC='\033[0m' # No color
 
-
-IFS='' read -r -d '' banner <<"EOF"
-#=======================================================================================#
-#                   _                  _                          _        _            #
-#  __ _ _ _  _ _ __| |_ ___ _ __  __ _| |_ ___ _ _ _____ __ _____| |__  __| |__ ___ __  #
-# / _| '_| || | '_ \  _/ _ \ '  \/ _` |  _/ _ \ '_|___\ V  V / -_) '_ \/ _` / _` \ V /  #
-# \__|_|  \_, | .__/\__\___/_|_|_\__,_|\__\___/_|      \_/\_/\___|_.__/\__,_\__,_|\_/   #
-#         |__/|_|                                                                       #
-#---------------------------------------------------------------------------------------#
-EOF
-echo -en "${C_GREEN}${banner}${C_NC}"
+printf "${C_GREEN}#=======================================================================================#${C_NC}\n"
+printf "${C_GREEN}#                   _                  _                          _        _            #${C_NC}\n"
+printf "${C_GREEN}#  __ _ _ _  _ _ __| |_ ___ _ __  __ _| |_ ___ _ _ _____ __ _____| |__  __| |__ ___ __  #${C_NC}\n"
+printf "${C_GREEN}# / _| '_| || | '_ \  _/ _ \ '  \/ _\` \|  _/ _ \ '_|___\ V  V / -_) '_ \/ _\` / _\` \ V / #${C_NC}\n"
+printf "${C_GREEN}# \__|_|  \_, | .__/\__\___/_|_|_\__,_|\__\___/_|      \_/\_/\___|_.__/\__,_\__,_|\_/   #${C_NC}\n"
+printf "${C_GREEN}#         |__/|_|                                                                       #${C_NC}\n"
+printf "${C_GREEN}#---------------------------------------------------------------------------------------#${C_NC}\n"
 
 if [ -n "${CRYPTOMATOR_UID}" ]; then
-    echo -e "${C_GREEN}#${C_MAGENTA} Updating user ${CRYPTOMATOR_USER} UID to match env supplied UID (${CRYPTOMATOR_UID})...${C_NC}"
+    printf "${C_GREEN}#${C_MAGENTA} Updating user ${CRYPTOMATOR_USER} UID to match env supplied UID (${C_GREEN}${CRYPTOMATOR_UID}${C_MAGENTA})...${C_NC}\n"
     usermod --uid "${CRYPTOMATOR_UID}" "${CRYPTOMATOR_USER}" | grep -v 'usermod: no changes'
 fi
 
 if [ -n "${CRYPTOMATOR_GID}" ]; then
-    echo -e "${C_GREEN}#${C_MAGENTA} Updating group ${CRYPTOMATOR_USER} GID to match env supplied GID (${CRYPTOMATOR_GID})...${C_NC}"
+    printf "${C_GREEN}#${C_MAGENTA} Updating group ${CRYPTOMATOR_USER} GID to match env supplied GID (${C_GREEN}${CRYPTOMATOR_GID}${C_MAGENTA})...${C_NC}\n"
     groupmod --gid "${CRYPTOMATOR_GID}" "${CRYPTOMATOR_USER}"
-    echo -e "${C_GREEN}#${C_MAGENTA} Updating user ${CRYPTOMATOR_USER}'s default group to match env supplied GID (${CRYPTOMATOR_GID})...${C_NC}"
+    printf "${C_GREEN}#${C_MAGENTA} Updating user ${CRYPTOMATOR_USER}'s default group to match env supplied GID (${C_GREEN}${CRYPTOMATOR_GID}${C_MAGENTA})...${C_NC}\n"
     usermod --gid "${CRYPTOMATOR_GID}" "${CRYPTOMATOR_USER}" | grep -v 'usermod: no changes'
 fi
 
 if [ -n "${CRYPTOMATOR_UMASK}" ]; then
-    echo -e "${C_GREEN}#${C_MAGENTA} Setting env supplied file umask (${CRYPTOMATOR_UMASK})...${C_NC}"
+    printf "${C_GREEN}#${C_MAGENTA} Setting env supplied file umask (${C_GREEN}${CRYPTOMATOR_UMASK}${C_MAGENTA})...${C_NC}\n"
     umask "${CRYPTOMATOR_UMASK}"
 fi
 
-echo -e "${C_GREEN}#${C_MAGENTA} Dropping privileges...${C_NC}"
+printf "${C_GREEN}#${C_MAGENTA} Dropping privileges and executing ${C_GREEN}/entrypoint.sh${C_MAGENTA}...${C_NC}\n"
+
 # Don't use 'su', see http://jdebp.info/FGA/dont-abuse-su-for-dropping-privileges.html
-setpriv --reuid="${CRYPTOMATOR_UID}" --regid="${CRYPTOMATOR_GID}" --clear-groups -- bash -c '/entrypoint.sh'
+/usr/bin/setpriv --reuid="${CRYPTOMATOR_UID}" --regid="${CRYPTOMATOR_GID}" --clear-groups -- sh -c '/entrypoint.sh' &
+CRYPTOMATOR_ENTRYPOINT_PID=$!
+
+printf "${C_GREEN}#${C_MAGENTA} entrypoint.sh PID: ${C_GREEN}${CRYPTOMATOR_ENTRYPOINT_PID}${C_NC}\n"
+
+# Wait for the entrypoint command to finish, run the cleanup if it dies
+wait $CRYPTOMATOR_ENTRYPOINT_PID
+cleanup EXIT
